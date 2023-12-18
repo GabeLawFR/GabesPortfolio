5dafc3efb0e7c14e38d54a2f83227f774e79fcb3
GabeLawFR <gabriel.lawson@live.fr>
2023-12-18 11:20:14 -0800

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+  return <div>
+    <Navbar />
+  </div>
 }
 
-export default App
+export default App;
